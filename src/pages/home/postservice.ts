a8d583b0f08159add2e8adf9c94a84496d1ee553
wemrekurt <emre.kurt@bil.omu.edu.tr>
2017-04-18 19:58:56 +0300

    Request service

--- src/pages/home/postservice.ts
+++ src/pages/home/postservice.ts
@@ -1,10 +1,35 @@
 import {Injectable, Inject} from '@angular/core';
 import {Http, Headers} from '@angular/http';
+import {AuthService} from '../home/authservice';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PostService {
-    constructor(public http: Http) {
+
+
+    constructor(public http: Http, public authservice: AuthService) {
         this.http = http;
     }
+
+
+    getPage(page) {
+        return new Promise(resolve => {
+            var headers = new Headers();
+            this.authservice.loadUserCredentials();
+            headers.append('Authorization', 'Bearer: ' +this.authservice.AuthToken);
+            this.http.get('http://localhost/bgp/api/'+page+'.json', {headers: headers}).map(res => res.json()).subscribe(data => {
+                if(data.response == 'success'){
+                    resolve(data);                    
+                }else{
+                    resolve(false);
+                }
+            });
+        });
+    }
+
+
+
+
+    
 
 }
