179ef23157bfd2e02035ecc2a7593276773b7805
wemrekurt <emre.kurt@bil.omu.edu.tr>
2017-04-19 18:49:53 +0300

    Projeler Sayfası tamamlandı

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AuthService } from '../home/authservice';
 import { PostService } from '../home/postservice';
 import { HomePage } from '../home/home';
 
@@ -18,18 +17,49 @@ import { HomePage } from '../home/home';
 })
 export class Projects {
 
-  constructor(public navCtrl: NavController) {
+  public all_projects: any;
+  public filtered_projects: any;
+  searchQuery: string = '';
+
+  constructor(public navCtrl: NavController, public postservice: PostService) {
 
     if(window.localStorage.getItem('globalmedia')) {
-            
+      this.postservice.getPage('projects').then(res => { 
+        this.all_projects = res['data']['projects']; 
+        this.filtered_projects = res['data']['projects']; 
+      });
     }else{
       this.navCtrl.setRoot(HomePage);
     }
 
   }
 
+  initializeItems(){
+    this.filtered_projects = this.all_projects;
+  }
+
+  getItems(ev: any) {
+    // refill projects
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.filtered_projects = this.all_projects.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  /*
+  loadPage(id){
+    this.navCtrl.push(Projectpage,{ projectId: id });
+  }
+  */
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Projects');
   }
 
 }
